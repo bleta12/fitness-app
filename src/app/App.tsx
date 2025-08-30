47322a27f11c6c74126b78b06be80b5a93771354
bleta12 <102874311+bleta12@users.noreply.github.com>
2025-08-30 21:02:48 +0200

    revert changes

--- src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/App.tsx
-import { useRoutes } from "react-router-dom";
-import { routes } from "./routes";
-
-const App = () => {
-    return useRoutes(routes);
-};
-
-export default App;
