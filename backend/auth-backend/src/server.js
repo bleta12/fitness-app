e70a5f92d5a5c94c6e6c56ea2f7d925e2b555df5
Teos Hoxha <th67329@ubt-uni.net>
2025-09-29 01:03:33 +0200

    config db file fixed

--- backend/auth-backend/src/server.js
+++ backend/auth-backend/src/server.js
@@ -1,26 +1,8 @@
-//import express from "express";
-//import cors from "cors";
-//import authRoutes from "../routes/auth.js";
-//
-//const app = express();
-//
-//app.use(cors());
-//app.use(express.json());
-//
-//app.use("/auth", authRoutes);
-//
-//app.get("/",(req, res) => {
-//    res.send("Api is running")
-//})
-//
-//app.listen(5000, () => {
-//    console.log("✅ Server running on http://localhost:5000");
-//});
 import express from "express";
 import cors from "cors";
 import authRoutes from "../routes/auth.js";
 import aiRouter from "./modules/ai/ai.resource.js";
-import mysqlDB from "./db/mysql.js";
+import mysqlDB from "../src/config.js";
 import "./db/mongo.js";
 
 const app = express();
